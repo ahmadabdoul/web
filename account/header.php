66b9f9af4bad3cb95aae6d6f73a52383963c8568
Abdulkadir Ahmad <aabdulkadir109@gmail.com>
2023-09-07 06:29:31 -0700

    bootstrap added

--- account/header.php
+++ account/header.php
@@ -18,6 +18,8 @@ if (!isset($_SESSION['user_id'])) {
   <title>User Dashboard</title>
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="stylesheet" href="styles.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css
+" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" />
 </head>
